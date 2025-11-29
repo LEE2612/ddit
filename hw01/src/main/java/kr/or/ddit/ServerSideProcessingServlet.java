ef0c292cf4799215b9c41ec60ae0b03a8f98df74
LEE2612 <gusrbwwkd9@gmail.com>
2025-11-29 17:46:19 +0900

    2025/11/29 LEE 과제 진행중....

--- hw01/src/main/java/kr/or/ddit/ServerSideProcessingServlet.java
+++ hw01/src/main/java/kr/or/ddit/ServerSideProcessingServlet.java
@@ -13,7 +13,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 /*
-	2025/11/28 이현규 사칙연산 part1
+	2025/11/28 이현규 사칙연산 part2
 	- jsp 입력페이지와 사칙연산기능을 수행하는 서블릿 제작
 	- jsp 페이지 제작전에 테스터 외부 api를 사용하여 작동 여부 확인 및 오류코드 처리 정상 작동 확인
 	- jsp 페이지는 webapp안의 calc/calculate폴더 경로에 생성 이름은 calc.jsp
@@ -21,6 +21,10 @@ import jakarta.servlet.http.HttpServletResponse;
 	- ex) 1 + 2 결과값 경로 http://localhost/hw02/calc/calculate?left=1&operator=%2B&right=2
 	- 싱글페이지가 아니라 멀티페이지로 구현 / 입력창 => 결과창
 	- 상태코드 구현 완료 = 값이 입력되지 않았을때, 입력받은 값이 숫자가 아닐때, 기호가 잘못되었을때, 결과값이 올바르지 않을때
+	===================part1==================
+	- 체크박스 html or json 출력 형식 분리
+	- jsp에서 유효성 검사 실행
+	- 
  */
 
 @WebServlet("/calc/calculate")
@@ -29,36 +33,23 @@ public class ServerSideProcessingServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
 			throws ServletException, IOException {
-		//		System.out.println("수신 받았쪄"); // 수신 잘되는지 콘솔 로그 출력
+		// 1. 데이터 선언 영역
+
 		String left = req.getParameter("left"); // 외부 요청 수신 받기 (테스터)
 		String right = req.getParameter("right");
 		String operator = req.getParameter("operator");
+		String accept = req.getParameter("accept");
 
-		if(left == null || left =="" ||
-		   right == null || right == "") {
+		if(left.equals("") || right.equals("")) {
 			resp.sendError(400, "값이 입력되지 않았습니다");
 			return;
 		}
-		
-		//		System.out.println(left); // 테스터 외부 app로 받은 문자열값 잘 나오는지 출력
-		//		System.out.println(right);
-		//		System.out.println(operator);
 
 		// 받은 문자열값 숫자로 변환
-		int num1;
-		int num2;
-		try {
-			num1 = Integer.parseInt(left); 
-			num2 = Integer.parseInt(right);
-		} catch (NumberFormatException e) {
-			resp.sendError(400, "입력된 값이 숫자가 아닙니다.");
-			return;
-		}
+		int num1 = Integer.parseInt(req.getParameter("left"));
+		int num2 = Integer.parseInt(req.getParameter("right"));
 
-		
-
-		//		System.out.println(num1); // 숫자로 변환 잘됐는지 확인용 변환값 출력
-		//		System.out.println(num2);
+		// 2. 함수 영역
 
 		// 스위치 문으로 변환한 숫자들을 이용해서 연산 처리
 		int result = 0; // 결과값용 int
@@ -67,23 +58,48 @@ public class ServerSideProcessingServlet extends HttpServlet {
 		case "-" : result = num1 - num2; break;
 		case "*" : result = num1 * num2; break;
 		case "/" : result = num1 / num2; break;
-		default: resp.sendError(400, "잘못된 입력입니다");
-//		System.out.println("잘못된 입력입니다");
-		return;
+		default  : break;
 		}
 
-		System.out.println(num1 + operator + num2 + "="+ result); // 숫자값, 연산기호, 연산식, 결과값 잘 나오는지 확인
-
-		resp.setContentType("text/html;charset=UTF-8"); // 응답 구문
-
-		if(resp.getStatus()==200) {
-			try (PrintWriter out = resp.getWriter()) { // 향상된 트라이 / 리소스문 사용
-				System.out.println("정상 출력 확인");
-				out.print("result: "+result); // 결과값 응답
-			}
-		}else {
-			resp.sendError(400);
-			System.out.println("잘못된 입력입니다");
+//		System.out.println(accept.getClass().getName());
+		// 3. 리턴 영역
+//		if(accept.equals("html")) { //html인경우
+//			resp.setContentType("text/html;charset=UTF-8");// 응답 구문
+//		}else { //json도 html도 아닌 경우
+//			resp.setContentType("application/json;");
+//		}
+//		
+//		if(resp.getStatus()==200) { 
+//			try (PrintWriter out = resp.getWriter()) { // 향상된 트라이 / 리소스문 사용
+//				System.out.println("정상 출력 확인");
+//				out.print("result: "+result); // 결과값 응답
+//			}
+//		}else {
+//			resp.sendError(400);
+//			System.out.println("잘못된 입력입니다");
+//		}
+		
+		if ("html".equals(accept)) {
+		    resp.setContentType("text/html;charset=UTF-8");
+		    try(PrintWriter out = resp.getWriter()) {
+		        out.printf("<h3>계산 결과: %d</h3>", result);
+		    }
+		} else { // json이거나 기본 처리
+		    resp.setContentType("application/json;charset=UTF-8");
+		    try(PrintWriter out = resp.getWriter()) {
+		        out.printf("{\"result\": %d}", result);
+		    }
 		}
+
+		
+		// resp.getWriter().write(gson);
+
+		// 결과 html, json -> 결과 result : "4" 
+		
+		// System.out.println(num1 + operator + num2 + "="+ result); // 숫자값, 연산기호, 연산식, 결과값 잘 나오는지 확인
+
+
+
+
 	}
 }
--- hw01/src/main/webapp/calc/calculate/calc.jsp
+++ hw01/src/main/webapp/calc/calculate/calc.jsp
@@ -5,19 +5,32 @@
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="${pageContext.request.contextPath}/resource/js/app/calculate.js"></script>
 </head>
 <body>
-<form action="/hw02/calc/calculate" method="get">
-<input type="text" name="left"/>
-<select name="operator">
-			<option>+</option>
-			<option>-</option>
-			<option>*</option>
-			<option>/</option>
-</select>
-<input type="text" name="right"/> <br>
-<input type="submit" value="전송">
-<h4>결과값: ${result }</h4>
-</form>
+    <form action="/hw02/calc/calculate">
+        <h4>수신 희망 컨텐츠 형식 선택</h4>
+        <label><input type="radio" name="accept" value="html" checked />HTML</label>
+        <label><input type="radio" name="accept" value="json" />JSON</label>
+
+        <br>
+        <input id="left" type="text" name="left"/>
+        <select id="operator" name="operator">
+            <option>+</option>
+            <option>-</option>
+            <option>*</option>
+            <option>/</option>
+        </select>
+        <input id="right" type="text" name="right"/> 
+        <br>
+
+        <button type="submit">전송</button>
+
+        <h4>결과값:</h4>
+    </form>
+
+    <div id="resultArea"></div>
+
 </body>
 </html>
