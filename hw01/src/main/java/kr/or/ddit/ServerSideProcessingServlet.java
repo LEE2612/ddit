290a98302cb93165bf6be31fc7111ddb0bbf6689
LEE2612 <gusrbwwkd9@gmail.com>
2025-11-29 11:47:47 +0900

    2025/11/29 LEE 오류코드 처리 구현 및 주석 작성 완료 값이 입력되지 않았을때, 값의 입력이 올바르지 않을때 ex) 숫자가 아니라 문자 입력
    오류코드 400 사용 정상작동 할때에는 상태코드 200으로 정상작동 처리
    외부 테스터 앱 Talend API Tester 을 이용하여
    오류구문 정상작동 및 출력 여부 확인 완료
    jsp페이지 오류 구문 정상 출력 확인 완료

--- hw01/src/main/java/kr/or/ddit/ServerSideProcessingServlet.java
+++ hw01/src/main/java/kr/or/ddit/ServerSideProcessingServlet.java
@@ -3,6 +3,8 @@ package kr.or.ddit;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.management.RuntimeErrorException;
+
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -13,35 +15,51 @@ import jakarta.servlet.http.HttpServletResponse;
 /*
 	2025/11/28 이현규 사칙연산 part1
 	- jsp 입력페이지와 사칙연산기능을 수행하는 서블릿 제작
-	- jsp 페이지 제작전에 테스터 외부 api를 사용하여 작동 여부 확인
+	- jsp 페이지 제작전에 테스터 외부 api를 사용하여 작동 여부 확인 및 오류코드 처리 정상 작동 확인
 	- jsp 페이지는 webapp안의 calc/calculate폴더 경로에 생성 이름은 calc.jsp
 	- http://localhost/hw02/calc/calculate/calc.jsp 입력 페이지 경로
 	- ex) 1 + 2 결과값 경로 http://localhost/hw02/calc/calculate?left=1&operator=%2B&right=2
-	- 싱글페이지가 아니라 멀티페이지로 구현
-	- 상태코드 구현
-*/
+	- 싱글페이지가 아니라 멀티페이지로 구현 / 입력창 => 결과창
+	- 상태코드 구현 완료 = 값이 입력되지 않았을때, 입력받은 값이 숫자가 아닐때, 기호가 잘못되었을때, 결과값이 올바르지 않을때
+ */
 
 @WebServlet("/calc/calculate")
 public class ServerSideProcessingServlet extends HttpServlet {
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
 			throws ServletException, IOException {
-//		System.out.println("수신 받았쪄"); // 수신 잘되는지 콘솔 로그 출력
+		//		System.out.println("수신 받았쪄"); // 수신 잘되는지 콘솔 로그 출력
 		String left = req.getParameter("left"); // 외부 요청 수신 받기 (테스터)
 		String right = req.getParameter("right");
 		String operator = req.getParameter("operator");
+
+		if(left == null || left =="" ||
+		   right == null || right == "") {
+			resp.sendError(400, "값이 입력되지 않았습니다");
+			return;
+		}
 		
-//		System.out.println(left); // 테스터 외부 app로 받은 문자열값 잘 나오는지 출력
-//		System.out.println(right);
-//		System.out.println(operator);
+		//		System.out.println(left); // 테스터 외부 app로 받은 문자열값 잘 나오는지 출력
+		//		System.out.println(right);
+		//		System.out.println(operator);
+
+		// 받은 문자열값 숫자로 변환
+		int num1;
+		int num2;
+		try {
+			num1 = Integer.parseInt(left); 
+			num2 = Integer.parseInt(right);
+		} catch (NumberFormatException e) {
+			resp.sendError(400, "입력된 값이 숫자가 아닙니다.");
+			return;
+		}
+
 		
-		int num1 = Integer.parseInt(left); // 받은 문자열값 숫자로 변환
-		int num2 = Integer.parseInt(right);
-		
-//		System.out.println(num1); // 숫자로 변환 잘됐는지 확인용 변환값 출력
-//		System.out.println(num2);
-		
+
+		//		System.out.println(num1); // 숫자로 변환 잘됐는지 확인용 변환값 출력
+		//		System.out.println(num2);
+
 		// 스위치 문으로 변환한 숫자들을 이용해서 연산 처리
 		int result = 0; // 결과값용 int
 		switch(operator) {
@@ -49,15 +67,23 @@ public class ServerSideProcessingServlet extends HttpServlet {
 		case "-" : result = num1 - num2; break;
 		case "*" : result = num1 * num2; break;
 		case "/" : result = num1 / num2; break;
-		default: System.out.println("정확한 값을 입력하십시오");
+		default: resp.sendError(400, "잘못된 입력입니다");
+//		System.out.println("잘못된 입력입니다");
+		return;
 		}
-		
+
 		System.out.println(num1 + operator + num2 + "="+ result); // 숫자값, 연산기호, 연산식, 결과값 잘 나오는지 확인
-		
+
 		resp.setContentType("text/html;charset=UTF-8"); // 응답 구문
-		
-		try (PrintWriter out = resp.getWriter()) { // 향상된 트라이 / 리소스문 사용
-			out.print("result: "+result); // 결과값 응답
+
+		if(resp.getStatus()==200) {
+			try (PrintWriter out = resp.getWriter()) { // 향상된 트라이 / 리소스문 사용
+				System.out.println("정상 출력 확인");
+				out.print("result: "+result); // 결과값 응답
+			}
+		}else {
+			resp.sendError(400);
+			System.out.println("잘못된 입력입니다");
 		}
 	}
 }
